eb541f0fdd1bc0460b197130b52263c25516ba81
Shubham Agrawal <58412969+shubham1206agra@users.noreply.github.com>
2022-06-13 13:28:40 +0530

    Update xeus-cling.hpp

--- binder/xeus-cling.hpp
+++ binder/xeus-cling.hpp
@@ -2,6 +2,6 @@
 #pragma cling load("/code/build/lib/libmlpack.so")
 #pragma cling load("/srv/conda/envs/notebook/lib/libomp.so")
 #pragma cling load("/srv/conda/envs/notebook/lib/libpython3.so")
-#pragma cling add_include_path("/srv/conda/envs/notebook/include/python3.9m/")
+#pragma cling add_include_path("/srv/conda/envs/notebook/include/python3.7m/")
 
 #define ARMA_DONT_USE_WRAPPER
